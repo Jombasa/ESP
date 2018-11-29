f6109ae12260603f0f321afde3f2689cfea37a7c
Tomaž Mesarec <tomaz.mesarec@gmail.com>
2018-11-29 21:02:02 +0100

    Enryption part

--- ass2.c
+++ ass2.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
+#include <string.h>
+
+
+
+const char * encryption(char *input_string)
+{
+  char ch;
+  int string_length = strlen(input_string);
+  int enryption_key = 256 % string_length;
+  ////encrypitng chars
+  for(int it = 0; input_string[it] != '\0'; ++it)
+  {
+    ch = input_string[it] + enryption_key;
+    //ch > 'z'
+    if(ch > 122)
+    {
+      //ch = ch - z + a -1
+      ch = ch - 122 + 97 -1;
+    }
+    input_string[it] = ch;
+  }
+  return input_string;
+}
 
 
 int main (void)
 {
-  
+  char input_string[255] = "bruteforce";
+
+  const char* encrypted_string = encryption(input_string);
+
+  printf("%s\n", encrypted_string);
+
+  return 0;
 }
